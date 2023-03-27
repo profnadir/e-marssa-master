f71295a41f2fe3cb09707782f9ecce4cb0233ab8
Hamza Nadir <hamzanadir.dev@gmail.com>
2023-03-27 16:18:51 +0000

    implements queue

--- app/Notifications/ProductCreated.php
+++ app/Notifications/ProductCreated.php
@@ -9,7 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Messages\VonageMessage;
 
-class ProductCreated extends Notification
+class ProductCreated extends Notification implements ShouldQueue
 {
     use Queueable;
 
